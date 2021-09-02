02ac228f43d697ebe7736164203d630570112e53
Oliwer <oe222ez@student.lnu.se>
2021-09-02 20:19:56 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import YourName from './components/YourName'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
-    <>
+    <div id="app">
     <div id="centered-box">
       <div>
         <YourName />
       </div>
     </div>
-    </>
+    <Footer />
+    </div>
   )
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <p><a target="_blank" without rel="noreferrer" href="http://open-notify.org/Open-Notify-API/ISS-Location-Now/">ISS position api</a> created by <a target="_blank" without rel="noreferrer" href="http://open-notify.org/">Open Notify</a></p>
+    </div>
+  )
+}
+
+export default Footer
