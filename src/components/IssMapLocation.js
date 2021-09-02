7a590bd0bdb03d6244aee9b8a2ebfe48b1d66686
Oliwer <oe222ez@student.lnu.se>
2021-09-02 19:58:24 +0200

    add google map in IssMapLocation component

--- src/components/IssMapLocation.js
+++ src/components/IssMapLocation.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import './iss-map-location.css'
+import GoogleMapReact from 'google-map-react'
+import MapMarker from './MapMarker'
 
 const IssMapLocation = () => {
+
+  
+
   return (
-    <>
-      <h1>ISS MAP!</h1>
-    </>
+    <div style={{ height: '100%', width: '100%' }} id="Iss-map location">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyABWPKtKCYlTKKBbHY6PReiBB99aR2lJzY' }}
+        defaultCenter={{ lat: 20.95, lng: 30.33}}
+        defaultZoom={5}
+      >
+        <MapMarker
+          lat={20.955413}
+          lng={30.337844}
+        />
+      </GoogleMapReact>
+      <a target="_blank" href="https://icons8.com/icon/13800/marker">Marker</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+    </div>
   )
 }
 
---
+++ src/components/MapMarker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import image from '../img/icons8-marker-48.png'
+
+const MapMarker = () => {
+  return (
+    <>
+      <img src={image} alt="Location" />
+    </>
+  )
+}
+
+export default MapMarker
